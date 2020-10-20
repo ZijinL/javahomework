8546efbda91ff71e89e547d08a3f9da9727eecc7
Zijin <zijin.luo@luozijin.com>
2020-10-20 08:49:55 +0800

    update homework_1021

--- homework_1021_5.java
+++ homework_1021_5.java
@@ -1,8 +1,15 @@
 import java.util.*;
 public class homework_1021_5 {
     public static void main(String [] args) {
-        double test [][] = {{1,2,3,4}, {1,2,3,4}, {4,3,2,1}, {2,3,4,3}};
-        System.out.println(sumMajorDiagonal(test));
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter a 4-by-4 matrix: ");
+        double test [][] = new double[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                test[i][j] = input.nextDouble();
+            }
+        }
+        System.out.println("Sum of the elements in the major diagonal is " + sumMajorDiagonal(test));
     }
     public static double sumMajorDiagonal (double[][]m) {
         double sum = 0;
